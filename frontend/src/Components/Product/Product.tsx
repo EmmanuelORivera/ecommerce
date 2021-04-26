3c2809d3dc90390e0e5618eac537175237f532fc
Emmanuel Ochoa <eorivera95@gmail.com>
2021-04-26 12:27:39 -0600

    Adjusting the cards css

--- frontend/src/Components/Product/Product.tsx
+++ frontend/src/Components/Product/Product.tsx
@@ -6,22 +6,22 @@ interface ProductProps {
 }
 const Product: FC<ProductProps> = ({ product }) => {
   return (
-    <div className='card'>
-    <div className='card__wrapper'>
-      <div className='card__image'>
-        <a href={`/product/${product._id}`}>
-          <img src={product.image} alt={product.name} />
-        </a>
+    <a href={`/product/${product._id}`}>
+      <div className='card'>
+        <div className='card__wrapper'>
+          <div className='card__image'>
+            <img src={product.image} alt={product.name} />
+          </div>
+          <div className='card__info '>
+            <strong>{product.name}</strong>
+            <p>
+              {product.rating} from {product.numReviews} reviews
+            </p>
+            <strong className='card__price'>$ {product.price}</strong>
+          </div>
+        </div>
       </div>
-      <div className='card__info '>
-        <strong>{product.name}</strong>
-        <p>
-          {product.rating} from {product.numReviews} reviews
-        </p>
-        <strong className='card__price'>$ {product.price}</strong>
-      </div>
-      </div>
-    </div>
+    </a>
   );
 };
 
