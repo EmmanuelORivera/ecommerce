006e14765d154c4bc2536c69719660b69929b3e0
Emmanuel Ochoa <eorivera95@gmail.com>
2021-04-29 21:46:02 -0600

    adding switch from react router dom and also a component to scroll to the top when changes the current component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,19 @@ import Footer from './Components/Footer';
 import Header from './Components/Header/Header';
 import Home from './Pages/Home';
 import Product from './Pages/Product';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ScrollToTop from './Components/Helper/ScrollToTop';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Header />
       <div className='wrapper'>
         <main>
-          <Route path='/' component={Home} exact />
-          <Route path='/product/:id' component={Product} />
+          <Switch>
+            <Route path='/' component={Home} exact />
+            <Route path='/product/:id' component={Product} />
+          </Switch>
         </main>
       </div>
       <Footer />
