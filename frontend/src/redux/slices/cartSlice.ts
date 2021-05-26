15bd68a9802d85bc897890886bd8be670cd2da88
Emmanuel Ochoa <eorivera95@gmail.com>
2021-05-25 23:24:28 -0600

    improvements to the cartSlice

--- frontend/src/redux/slices/cartSlice.ts
+++ frontend/src/redux/slices/cartSlice.ts
@@ -1,8 +1,9 @@
 import axios, { AxiosError } from 'axios';
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { ValidationErrors } from './types';
+import { IBaseState, ValidationErrors } from './types';
 import { RootState } from '../store';
 import { IProduct } from '../../products';
+import HTTP_STATUS from '../enum';
 
 interface ICartProduct {
   product: string;
@@ -13,37 +14,58 @@ interface ICartProduct {
   quantity: number;
 }
 
-interface IPayload {
-  product: ICartProduct;
+interface IProductState extends IBaseState {
+  cartItems: Array<ICartProduct>;
 }
 
-interface IProductState {
-  cartItems: Array<IPayload>;
-}
-
-const addToCart = createAsyncThunk<
-  IPayload,
+export const addToCart = createAsyncThunk<
+  Array<ICartProduct>,
   { id: string; quantity: number },
   { rejectValue: ValidationErrors; state: RootState }
 >('cart/addToCart', async ({ id, quantity }, { rejectWithValue, getState }) => {
   try {
     const { data }: { data: IProduct } = await axios.get(`/api/products/${id}`);
-    const cartProduct: IPayload = {
-      product: {
-        product: data._id,
-        name: data.name,
-        image: data.image,
-        price: data.price,
-        countInStock: data.countInStock,
-        quantity,
-      },
+
+    const cartProduct: ICartProduct = {
+      product: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      quantity,
     };
 
-    localStorage.setItem(
-      'cartItems',
-      JSON.stringify(getState().cart.cartItems)
+    const state = getState().cart;
+
+    const findFetchedItemOnCurrentState = state.cartItems.find(
+      (cartItem) => cartItem.product === cartProduct.product
     );
-    return cartProduct;
+
+    const returnCartItems = () => {
+      if (findFetchedItemOnCurrentState) {
+        //make a copy and update only the ( CARTITEM ) that needs to be changed
+        const { cartItems } = {
+          ...state,
+          cartItems: state.cartItems.map((cartItem) =>
+            cartItem.product === findFetchedItemOnCurrentState.product
+              ? cartProduct
+              : cartItem
+          ),
+        };
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
+        return cartItems;
+      } else {
+        //just adding the new product to the array without a mutation
+        const { cartItems } = {
+          ...state,
+          cartItems: [...state.cartItems, cartProduct],
+        };
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
+        return cartItems;
+      }
+    };
+
+    return returnCartItems();
   } catch (err) {
     let error: AxiosError<ValidationErrors> = err;
     if (!error.response) {
@@ -53,29 +75,44 @@ const addToCart = createAsyncThunk<
   }
 });
 
-const initialState: IProductState = { cartItems: [] };
+const initialState: IProductState = {
+  cartItems: [],
+  status: HTTP_STATUS.IDLE,
+  errorMessage: '',
+};
 
 const cartSlice = createSlice({
   initialState,
   name: 'cart',
   reducers: {
-    addItem: (state, action: PayloadAction<IPayload>) => {
+    addItem: (state, action: PayloadAction<ICartProduct>) => {
       const item = action.payload;
       const existItem = state.cartItems.find((x) => x.product === item.product);
 
       if (existItem) {
-        const test = state.cartItems.map((x) =>
-          x.product === existItem.product ? item : x
+        const arrayCartItems = state.cartItems.map((cartItem) =>
+          cartItem.product === existItem.product ? item : cartItem
         );
-        state.cartItems = test;
+        state.cartItems = arrayCartItems;
       } else {
         state.cartItems.push(item);
       }
     },
   },
   extraReducers: (builder) => {
+    builder.addCase(addToCart.pending, (state) => {
+      state.status = HTTP_STATUS.IDLE;
+    });
     builder.addCase(addToCart.fulfilled, (state, action) => {
-      state.cartItems.push(action.payload);
+      state.cartItems = action.payload;
+    });
+    builder.addCase(addToCart.rejected, (state, action) => {
+      state.status = HTTP_STATUS.REJECTED;
+      if (action.payload) {
+        state.errorMessage = action.payload.message;
+      } else {
+        state.errorMessage = action.error.message;
+      }
     });
   },
 });
